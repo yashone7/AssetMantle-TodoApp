d7d2eed7fd3211ffeb869941163472fff6a93d43
Yashwanth Somayajula <yashwanth.somayajula16@gmail.com>
2023-09-25 19:20:08 +0530

    configured cors and morgan in app

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ import dotenv from "dotenv";
 import chalk from "chalk";
 import { connectDB } from "./db/db.js";
 import api from "./routes/api.js";
+import cors from "cors";
+import morgan from "morgan";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
 
 connectDB();
 
