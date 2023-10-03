1b62a2a67a0566aa103a0431221b5734d5a3c41b
Yashwanth Somayajula <yashwanth.somayajula16@gmail.com>
2023-10-03 20:03:54 +0530

    updated DB uri and preparing to deploy

--- db/db.js
+++ db/db.js
@@ -1,8 +1,19 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export async function connectDB() {
   try {
-    await mongoose.connect("mongodb://0.0.0.0:27017/todoApp");
+    let mongodbUri = "";
+
+    if (process.env.APP_ENV === "LOCAL") {
+      mongodbUri = process.env.MONGODB_URI_LOCAL;
+    } else {
+      mongodbUri = process.env.MONGODB_URI_CLOUD;
+    }
+
+    await mongoose.connect(mongodbUri);
     console.log("connected to MongoDB");
   } catch (err) {
     console.log(err);
